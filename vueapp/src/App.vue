0e2d4e56c874256ff397f1394a24894d46d286ea
李硕 <3011329583@qq.com>
2023-05-20 17:51:23 +0800

    Add project files.

---
+++ vueapp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <HelloWorld />
+    默认按钮
+    <!--<el-button class="primary">默认按钮</el-button>-->
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import HelloWorld from './components/HelloWorld.vue';
+
+    export default defineComponent({
+        name: 'App',
+        components: {
+            HelloWorld
+        }
+    });
+</script>
+
+<style>
+    #app {
+    }
+</style>
---
+++ vueapp/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+const app = createApp(App);
+app.mount('#app');
+
+//×¢²áElementUI
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
+app.use(ElementPlus)
+    .config.compilerOptions.isCustomElement = tag => tag.startsWith('el-')
+    ;
